c30cf651300ce0d8ad68e25cde7c6da3e1a17d88
angelascerbo <amscerbo@gmail.com>
2018-06-02 13:34:11 -0400

    loading component

---
+++ src/Loading.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { findDOMNode } from 'react-dom';
+import { TweenMax, SlowMo } from 'gsap';
+
+class Loading extends React.Component {
+  componentDidMount() {
+    TweenMax.staggerTo('.dot', 2, {
+      x:220,
+      backgroundColor:'#ca68fd',
+      repeat:-1,
+      ease:SlowMo.ease.config(0.5,0.4,false)
+    }, 0.4);
+
+    TweenMax.staggerFrom('.dot', 2, {
+      opacity:0,
+      scale:0.7,
+      repeat:-1,
+      ease:SlowMo.ease.config(0.5,0.4,true)
+    }, 0.4);
+  }
+
+  render() {
+    return (
+      <div className="loading-container">
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+      </div>
+    )
+  }
+}
+
+export default Loading;
